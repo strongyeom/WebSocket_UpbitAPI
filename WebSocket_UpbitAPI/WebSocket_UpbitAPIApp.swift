d6014a826aa0992bd357176d636eb816e515ef00
yeomsungpil <present7256@gmail.com>
2023-12-26 10:18:18 +0900

    Initial Commit

---
+++ WebSocket_UpbitAPI/WebSocket_UpbitAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocket_UpbitAPIApp.swift
+//  WebSocket_UpbitAPI
+//
+//  Created by 염성필 on 2023/12/26.
+//
+
+import SwiftUI
+
+@main
+struct WebSocket_UpbitAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
